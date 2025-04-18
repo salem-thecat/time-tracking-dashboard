76230424eab834fc761aaf8f49eb5a11cfea1386
salem-thecat <theslamec@gmail.com>
2025-04-18 23:55:52 +0300

    Добавил data.json и подключил JS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log("JS loaded");
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // Проверка — открой DevTools (F12) и глянь в консоль
+  });
